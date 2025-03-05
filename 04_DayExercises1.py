a6495b3b6f82f59bf798f67a32b735b624eb34c2
UP240492 <valtierraruelasd@gmail.com>
2025-03-05 15:31:53 -0600

    dia 4

---
+++ 04_DayExercises1.py
@@ -0,0 +1,44 @@
+#EJERCICIO 1
+WORD1, WORD2, WORD3, WORD4 = 'THIRTY', 'DAYS', 'OF', 'PYTHON'
+SINGLE_STRING = WORD1 + ''+ WORD2 + '' + WORD3 + '' + WORD4 
+print('La oracion es:',SINGLE_STRING)
+
+#EJERCICIO 2
+word1, word2, word3 = 'Coding', 'For', 'All'
+single_strings = word1 + word2 + word3
+print('la oracion es:',single_strings)
+
+#EJERCICIO 3
+empresa = 'Coding for all'
+
+#EJERCICIO 4
+print(empresa)
+
+#Ejercicio 5
+print('ejercicio 5')
+print(len(empresa))
+
+#EJERCICIO 6
+palabra = 'empresa'
+word = palabra.upper()
+print(word)
+
+#EJERCICIO 7
+palabra = 'EMPRESA'
+word_b = palabra.lower()
+print(word_b)
+
+#EJERCICIO 8
+word_c = 'Dios te ama'
+print(word_c.capitalize())
+print(word_c.title())
+print(word_c.swapcase())
+
+#EJERCICIO 9
+palabra = empresa.replace('Coding','')
+print(palabra)
+
+
+
+
+
