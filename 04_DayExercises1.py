46ec3dcd236caf8f7cc47714849465b1c722d403
UP240492 <valtierraruelasd@gmail.com>
2025-03-10 15:35:42 -0600

    DD

--- 04_DayExercises1.py
+++ 04_DayExercises1.py
@@ -35,8 +35,80 @@ print(word_c.title())
 print(word_c.swapcase())
 
 #EJERCICIO 9
+empresa= 'Coding For All'
 palabra = empresa.replace('Coding','')
 print(palabra)
+
+#EJERCICIO 10
+if empresa.find('Coding') == 0:
+    print(True)
+else:
+    print(False)
+
+#EJERCICIO 11
+replaceWord = empresa.replace("Coding , Python")
+print(replaceWord)
+
+#EJERCICIO 12
+palabra1 = 'PYTHON FOR EVERYONE'
+palabra2 = palabra1.replace('PYTHON FOR ALL','')
+print(palabra2)
+
+#EJERCICIO 13
+print('ejercicio 13')
+print(palabra1.split())
+
+#EJERCICIO 14
+print('ejercicio 14')
+wxw = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+p1q = wxw.split()
+print(p1q)
+
+#EJERCICIO 15
+print('ejercicio 15')
+ww = empresa[0]
+print(ww)
+
+#EJERCICIO 16
+print('ejercicio 16')
+zz = empresa[-1]
+print(zz)
+
+#EJERCICIO 17
+print('ejercicio 17')
+xx = empresa[10]
+print(xx)
+
+#EJERCICIO 18 
+print('ejercicio 18')
+rrr = "Python for everyone"
+acronimo = "".join(word[0] for word in rrr.split())
+print(acronimo)
+
+#EJERCICIO 19
+print('ejercicio 19')
+eee = 'Coding for all'
+acronimo2 = "",join(word[0] for word in eee.split())
+print(acronimo2)
+
+#EJERCICIO 2O
+print('ejercicio 20')
+ttt = 'Coding For All'
+print(ttt.find('C'))
+
+#EJERCICIO 21
+print('ejecicio 21')
+print(ttt.find('F'))
+
+#EJERCICIO 22
+print('ejercicio 22')
+print(ttt.find('l'))
+
+#EJERCICIO 23
+jjj = 'You cannot end a sentence with because because because is a conjunction'
+print(jjj.find('because'))
+
+#EJERCICIO 24
 
 
 
